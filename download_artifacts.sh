dc84457368473bd022ca93f57cfe06c4511e50ea
Ethan Apodaca <papodaca@gmail.com>
2018-03-20 23:27:10 -0700

    clean up

--- download_artifacts.sh
+++ download_artifacts.sh
@@ -5,13 +5,16 @@ repos=( "Javafx-WebView-Debugger" "monaco.editor" )
 rm -r artifacts
 mkdir artifacts
 
-for index in {0..1}
+alias curl="curl –silent"
+
+for repo in "${repos[@]}"
 do
-  repo=${repos[$index]}
   build_info=$(curl "https://circleci.com/api/v1.1/project/github/CueNinja/${repo}?circle-token=${CI_TOKEN}&limit=1&filter=completed")
   build_num=$(echo $build_info | jq ". [0] .build_num")
+  build_num=`expr $build_num - 1`
   artifact_info=$(curl "https://circleci.com/api/v1.1/project/github/CueNinja/${repo}/${build_num}/artifacts?circle-token=${CI_TOKEN}")
   artifact_url=$(echo $artifact_info | jq ".[] .url" | sed "s/\"//g")
+  echo "Downloading ${artifact_url}"
   curl -o artifacts/${repo}.tar.gz "${artifact_url}?circle-token=${CI_TOKEN}"
 
   mkdir artifacts/${repo}/
